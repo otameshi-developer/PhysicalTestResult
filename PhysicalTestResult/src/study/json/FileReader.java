d8eeb9049aa78abec3653cc3680ad0fc7fda798a
hkaneko <kanegongon1610@gmail.com>
2020-05-26 16:44:39 +0900

    FileReader.javaの指摘修正

--- PhysicalTestResult/src/study/json/FileReader.java
+++ PhysicalTestResult/src/study/json/FileReader.java
@@ -1,36 +1,35 @@
 package study.json;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 /**
  * ファイルを読み込み、文字列を生成する。
  *
  */
-public class FileReader { // TODO: otsuka: あんまりこういうクラス名ってみないよね。pure javaのjavadocとか参考にしてほしいんだけど、この場合はFileReaderじゃない？（読む人はreadじゃなくてreaderじゃない？） kaneko: 確かにFileReaderです。FileReadから変更しました。
+public class FileReader {
 
 	/**
-	 * ファイルを読み込み、文字列を生成する。
+	 * ファイルを読み込み、文字列を返却する。
 	 *
 	 * @param filePath ファイルのパス
-	 * @return ファイルから生成した文字列 // TODO: otsuka: なにか変換したの？　kaneko: 変換はしていないです。「ファイルから生成した文字列」に変更します。
+	 * @return ファイルから読み込んだ文字列 
 	 * @throws IOException 読み込みに失敗
 	 */
 	public String read(String filePath) throws IOException {
 
 		StringBuilder fileData = new StringBuilder();
-		// TODO: otsuka: main()で実行したときって、リソースこうやって取らないといけないんだっけ？なんか普通にFileインスタンス生成すればよいような気もするんだけど。。。（MEAPはこの取り方が正解！） kaneko: Fileインスタンス生成で出来たので修正しました。
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath)), StandardCharsets.UTF_8))) {
-			String line = br.readLine();
-			while (line != null) {
+
+		Path path = Paths.get(filePath);
+		try (Stream<String> stream = Files.lines(path,  StandardCharsets.UTF_8)) {
+			stream.forEach(line -> {
 				fileData.append(line);
-				line = br.readLine();
-			}
-		}// TODO: otsuka: これってなんでキャッチしているの？意味ある？ kaneko: 上に投げるだけだったので意味なかったです。catch句は消しました。
+			});
+		}
 		return fileData.toString();
 
 	}
